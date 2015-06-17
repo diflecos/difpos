02f341c8d79a98a6aa5397abb25a2e2df51969a7
Antonio <antonio.alonso@diflecos.com>
2015-06-17 18:46:54 +0200

    categorySelection working ok

--- simplepos/client/views/itemlocator/itemlocator.js
+++ simplepos/client/views/itemlocator/itemlocator.js
@@ -7,37 +7,68 @@ Template.itemlocator.rendered=function() {
 	Session.set("categorySelection",new Array());
 }
 
+/* 
+Dado un array de objetos como el siguiente: 
+categorySelection=[
+  { "level": 0, "name": "Inicio", "category_id": "dST8LFTkRwRCkmrxK" },
+  { "level": 1, "name": "Partes de arriba", "category_id": "Y8dgxKCwp7fDWeLEK" },
+  { "level": 2, "name": "Blusas", "category_id": "6cNrxvxdXztNxWqQi" }
+]
+esta función ordena las entradas según el "level"
+*/
+function categorySelectionSortByLevel(categorySelection) {
+	if(categorySelection!=undefined) {
+		categorySelection.sort(function(a,b) {
+			return a.level-b.level;
+		});	
+	} 
+	return categorySelection;
+}
+
+/* Devuelve el parent de la última categoría seleccionada */
+function lastCategorySelectionParentId(categorySelection) {
+	if(categorySelection===undefined) {
+		return null;
+	} else if(categorySelection.length==0) {
+		return null;
+	} else {			
+		categorySelectionSortByLevel(categorySelection);
+		return categorySelection[categorySelection.length-1].category_id;
+	}
+}
+
 Template.itemlocator.helpers({
 	selectedCategories: function() {
 		categorySelection=Session.get("categorySelection");	
-		if(categorySelection!=undefined) {
-			categorySelection.sort(function(a,b) {
-				return a.level-b.level;
-			});
-			
-			return Session.get("categorySelection");
-		} else {
-			return undefined;
-		}
+		return categorySelectionSortByLevel(categorySelection);
 	},
 	childCategories: function() { 
 		/* Recuperamos el parent a partir de la estructura de la Session, para ello ordenamos por level y cogemos el último */
 		categorySelection=Session.get("categorySelection");
-		if(categorySelection.length==0) {
-			parent=null;
-		} else {			
-			categorySelection.sort(function(a,b) {
-				return a.level-b.level;
-			});
-			parent=categorySelection[categorySelection.length-1].category_id;
+		return Categories.find({"parent": lastCategorySelectionParentId(categorySelection)});
+	},
+	isThereChildCategories: function() { 
+		categorySelection=Session.get("categorySelection");
+		return Categories.find({"parent": lastCategorySelectionParentId(categorySelection)}).count()>0;
+	}
+});
+
+Template.itemlocator.events({
+	"click a.categorySelected": function(event) {
+		level=event.target.dataset.level;  
+		aux=Session.get("categorySelection");	
+		categorySelection=new Array();
+		for(var i=0;i<aux.length;i++) {
+			if(aux[i].level<=level) {
+				categorySelection.push(aux[i]);
+			}
 		}
-		
-		return Categories.find({"parent": parent});
+		Session.set("categorySelection",categorySelection);
 	}
 });
 
 Template.categorySelector.events({
-	"click a": function() {
+	"click a.categorySelector": function(event) {
 		id=event.target.dataset.mongoid;  
 		catname=event.target.dataset.catname;
 		
@@ -45,7 +76,7 @@ Template.categorySelector.events({
 		if(categorySelection.length==0) 
 			level=0;
 		else	
-			level=Math.max(categorySelection.map(function(selcat){return selcat.level;}));
+			level=Math.max.apply(null,categorySelection.map(function(selcat){return selcat.level;}))+1;
 			
 		categorySelection.push({ "level": level, "name": catname,"category_id": id});
 		Session.set("categorySelection",categorySelection);
